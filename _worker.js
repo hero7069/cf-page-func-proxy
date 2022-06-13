ec9b38a6e4bc053976df34706bd10bd42875ab13
hero7069 <96361194+hero7069@users.noreply.github.com>
2022-06-13 10:04:55 +0800

    Create _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,11 @@
 export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="www.google.com";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
-      // Otherwise, serve the static assets.
-      return env.ASSETS.fetch(request);
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      url.hostname = 'mask2222.herokuapp.com'
+      let new_request = new Request(url, request);
+      return fetch(new_request);
     }
-  };
+    return env.ASSETS.fetch(request);
+  },
+};
