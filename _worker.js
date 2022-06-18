0865f3f18f091bf85c4f84a28f502fbd9072f66d
hero7069 <96361194+hero7069@users.noreply.github.com>
2022-06-18 12:40:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'mask2222.herokuapp.com'
+      url.hostname = 'psp4000.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
